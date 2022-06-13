f92246c5600769a5cffdb770db420bf662e33002
lee <sipossa@naver.com>
2022-06-13 16:06:28 +0900

    20220613

---
+++ js/slick01.js
@@ -0,0 +1,23 @@
+$(function () {
+
+
+    $('.viual_slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+    });
+
+
+    $('.slider').slick({
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 4000,
+        dots: true,
+        pauseOnHover: false,
+    });
+
+    $('.slider02').slick({
+        slidesToShow: 3,
+    });
+})
